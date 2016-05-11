bd3148b17e6cdd9c05691196c7b44d798bd77b75
zhangsiqi <276987359@qq.com>
2016-05-11 13:22:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# 小农女eslint配置
+
+配置中使用了[eslint推荐配置](http://eslint.org/docs/rules/)
+
+也使用了[eslint-plugin-react](https://github.com/yannickcr/eslint-plugin-react)
+
+###使用方法
+项目中新建.eslintrc.js
+```
+module.exports = {
+    "plugins": [
+        "gm"
+    ],
+    "extends": ["plugin:gm/recommended"]
+};
+```
+
+###配置方法
+rule配置http://eslint.org/docs/user-guide/configuring
+`"规则名": 0|1|2`
+* "off" or 0 - turn the rule off
+* "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
+* "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
+
+忽略单行检查
+`// eslint-disable-line`
+
+忽略多行检查
+```/*eslint-disable */
+     mutil line code
+/*eslint-enable */
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+    configs: {
+        recommended: {
+            "env": {
+                "browser": true,
+                "commonjs": true,
+                "es6": true,
+                "node": true
+            },
+            "parserOptions": {
+                "ecmaVersion": 7,
+                "ecmaFeatures": {
+                    "experimentalObjectRestSpread": true,
+                    "jsx": true
+                },
+                "sourceType": "module"
+            },
+            "plugins": [
+                "react"
+            ],
+            "parser": "babel-eslint",
+            "extends": ["eslint:recommended", "plugin:react/recommended"],
+            "rules": {
+                "no-console": 0,
+                "semi": 2,
+
+                // react 配置
+                "react/prop-types": 0, // 检查react props的属性是否在propTypes声明
+
+                "eqeqeq": 2
+            }
+        }
+    }
+};
